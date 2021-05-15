8881fb723813db786957c67ee177fdefb643a68f
VictoriaTaranenko <t.victoria-dev@mail.ru>
2021-05-15 17:50:38 +0300

    add homework3

--- src/com/geekbrains/homework3/Main.java
+++ src/com/geekbrains/homework3/Main.java
@@ -12,7 +12,6 @@ public class Main {
         String[] util = {"Яблоко","Яблоко","Яблоко","Сыр","Сыр","Сыр","Сыр",
                 "Говядина","Свинья","Свинья"};
 
-
         filterWordsByCounts(util);
     }
 
